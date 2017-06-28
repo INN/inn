b7ee54cab9671af0b9d6bb7b4799373fd56de261
Ben Keith <ben@benlk.com>
2017-06-28 12:48:00 -0400

    fixing placeholder and labels in partials/email-signup.php

--- partials/email-signup.php
+++ partials/email-signup.php
@@ -6,7 +6,7 @@
 			<p>News from INN and our members. <strong>Delivered weekly.</strong></p>
 
 			<form action="//inn.us1.list-manage.com/subscribe/post?u=81670c9d1b5fbeba1c29f2865&amp;id=19bec3393e" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-			    <div id="mc_embed_signup_scroll">
+				<div id="mc_embed_signup_scroll">
 					<div class="mc-field-group">
 						<input type="email" placeholder="email address" name="EMAIL" class="required email" id="mce-EMAIL">
 						<label for="EMAIL" class="visuallyhidden">email address</label>
@@ -14,9 +14,9 @@
 					<div id="mce-responses" class="clear">
 						<div class="response" id="mce-error-response" style="display:none"></div>
 						<div class="response" id="mce-success-response" style="display:none"></div>
-					</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-				    <div style="position: absolute; left: -5000px;"><input type="text" name="b_81670c9d1b5fbeba1c29f2865_19bec3393e" tabindex="-1" value=""></div>
-				    <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="btn">
+					</div>	<!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+					<div style="position: absolute; left: -5000px;"><input type="text" name="b_81670c9d1b5fbeba1c29f2865_19bec3393e" tabindex="-1" value=""></div>
+					<input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="btn">
 				</div>
 			</form>
 		</div>
