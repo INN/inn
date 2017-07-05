f8f2339b479a99b9ceacf94b887553894864a833
Ben Keith <ben@benlk.com>
2017-07-05 11:48:42 -0400

    Remove footer-social buttons, re-addFooter Navigation menu

--- partials/footer-boilerplate.php
+++ partials/footer-boilerplate.php
@@ -1,16 +1,12 @@
 <div id="boilerplate">
 	<div class="row-fluid clearfix">
 		<div class="span8 right">
-			<ul id="footer-social" class="social-icons">
-				<?php largo_social_links(); ?>
-			</ul>
 			<div class="footer-bottom clearfix">
 
-				<p class="footer-credit"><?php largo_copyright_message(); ?></p>
 				<?php do_action('largo_after_footer_copyright'); ?>
 				<?php largo_nav_menu(
 					array(
-						'theme_location' => 'footer-bottom',
+						'theme_location' => 'footer-navigation',
 						'container' => false,
 						'depth' => 1
 					) );
@@ -21,14 +17,21 @@
 
 	<div class="row-fluid clearfix">
 		<div class="span8 right">
-			<?php if (INN_MEMBER) { ?>
-				<?php inn_logo(); ?>
-			<?php } ?>
-			<p class="footer-credit <?php echo ( !INN_MEMBER ? 'footer-credit-padding-inn-logo-missing' : ''); ?>"><?php printf( __('Built with the <a href="%s">Largo WordPress Theme</a> from the <a href="%s">Institute for Nonprofit News</a>.', 'largo'),
+			<p class="footer-credit"><?php largo_copyright_message(); ?></p>
+			<p class="footer-credit <?php echo ( !INN_MEMBER ? 'footer-credit-padding-inn-logo-missing' : ''); ?>">
+				<?php printf( __('Built with the <a href="%s">Largo WordPress Theme</a> from the <a href="%s">Institute for Nonprofit News</a>.', 'largo'),
 					'http://largoproject.org',
 					'http://inn.org'
-				 );
-			?></p>
+				);
+				?>
+			</p>
+			<?php largo_nav_menu(
+				array(
+					'theme_location' => 'footer-bottom',
+					'container' => false,
+					'depth' => 1
+				) );
+			?>
 		</div>
 	</div>
 
--- partials/footer-widget-1col.php
+++ partials/footer-widget-1col.php
@@ -7,10 +7,4 @@
 	<?php inn_logo(); ?>
 	<p>714 W. Olympic Blvd. #931, Los Angeles, CA 90015</p>
 	<p><a href="tel:+18185823560">(818) 582-3560</a> <span class="sep">|</span> <a href="mailto:info@inn.org">info@inn.org</a></p>
-	<p class="footer-credit <?php echo ( !INN_MEMBER ? 'footer-credit-padding-inn-logo-missing' : ''); ?>"><?php printf( __('Built with the <a href="%s">Largo WordPress Theme</a> from the <a href="%s">Institute for Nonprofit News</a>.', 'largo'),
-			'http://largoproject.org',
-			'http://inn.org'
-		 );
-	?>
-	</p>
 </div>
