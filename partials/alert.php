c800b8302fa3b4fd12e0d8dda9ef008ed3f74761
tylermachado <tyler@inn.org>
2018-06-05 09:30:01 -0400

    change registration text to welcome text for INN Days alert

--- partials/alert.php
+++ partials/alert.php
@@ -1,8 +1,7 @@
 <div class="alert" style="background-color: #fff;">
 	<p>
 		<strong>
-			Accelerating nonprofit news: 
-			<a href="/2018/05/inn-days-2018/" style="color:#33BF95;">Register for INN Days June 13-14</a>
+			<a href="/2018/05/inn-days-2018/" style="color:#33BF95;">INN Days June 13-14:</a> Registration closed, see you in Orlando!
 		</strong>
 	</p>
 </div>
