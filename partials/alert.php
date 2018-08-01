ea48d0dd9e84c02d25ee8bb52294c3557f74d81f
tylermachado <tyler@inn.org>
2018-08-01 14:30:30 -0400

    new homepage alert for INN business intensive at Texas Tribune

--- partials/alert.php
+++ partials/alert.php
@@ -1,7 +1,7 @@
 <div class="alert" style="background-color: #fff;">
 	<p>
 		<strong>
-			<a href="/project/news-match/ " style="color:#09c9ff;">Apply to NewsMatch before August 1!</a>
+			Join us for <a href="//www.eventbrite.com/e/inn-business-intensive-audience-development-texas-tribune-tickets-47733484136Join us for " style="color:#09c9ff;">INN Business Intensive: Audience Development - Texas Tribune</a> on Sept. 11 + 12
 		</strong>
 	</p>
 </div>
