d15c6dd304bd848d1852b48ea5f788507d4a6db2
Ben Keith <ben@benlk.com>
2020-07-29 22:47:06 -0400

    Remove 'Hire Us' section from the homepage, and related CSS
    Resolves https://github.com/INN/umbrella-inndev/issues/169

--- homepages/templates/inn.php
+++ homepages/templates/inn.php
@@ -177,31 +177,6 @@
 
 <?php get_template_part('partials/programs'); ?>
 
-<section id="hire-us" class="interstitial">
-	<div class="content">
-		<h3>Need a little extra help?</h3>
-		<p>We offer affordable consulting services (at an even more discounted rate for members).</p>
-		<div class="row-fluid">
-			<div class="span4">
-				<img class="icon" src="<?php echo $img_path . 'icons/hire_webdev.svg'; ?>" />
-				<h5>Web Design + Development</h5>
-				<p>From full redesigns and CMS migrations to technology audits&mdash;and more.</p>
-			</div>
-			<div class="span4">
-				<img class="icon" src="<?php echo $img_path . 'icons/hire_strategy.svg'; ?>" />
-				<h5>Business + Growth Strategy</h5>
-				<p>Sustainable revenue and audience development for nonprofit news organizations.</p>
-			</div>
-			<div class="span4">
-				<img class="icon" src="<?php echo $img_path . 'icons/hire_apps.svg'; ?>" />
-				<h5>News Applications</h5>
-				<p>Data analysis and visualization for special editorial projects.</p>
-			</div>
-		</div>
-		<a class="btn btn-primary" href="/hire-us/">Learn more about hiring INN</a>
-	</div>
-</section>
-
 <?php get_template_part('partials/our_members'); ?>
 
 <?php get_template_part('partials/testimonials'); ?>
